c866a5affb0453b74faa46987ffc27c43b353ae5
Dongdong Tian <seisman.info@gmail.com>
2017-03-30 23:55:19 +0800

    setups

---
+++ seismanpy/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+
+__title__ = 'seismanpy'
+__version__ = '0.1.0'
+__author__ = 'Dongdong Tian'
+__license__ = 'MIT'
+__copyright__ = 'Copyright 2017 Dongdong Tian'
---
+++ setup.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import re
+from os import path
+from codecs import open
+from setuptools import setup
+
+here = path.abspath(path.dirname(__file__))
+
+# get the long description from the README file
+with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
+
+# get version number from __init__.py
+# https://github.com/kennethreitz/requests/blob/master/setup.py#L52
+with open('seismanpy/__init__.py', 'r') as fd:
+    version = re.search(r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]',
+                        fd.read(), re.MULTILINE).group(1)
+
+setup(
+    name='seismanpy',
+    version=version,
+    description='Tools for seismological researches',
+    long_description=long_description,
+    url='https://github.com/seisman/seismanpy',
+    author='Dongdong Tian',
+    author_email='seisman.info@gmail.com',
+    keywords='seismology',
+    license='MIT',
+
+    packages=['seismanpy'],
+    install_requires=[],
+    extras_require={
+        'dev': [
+            "codecov",
+            "coverage",
+            "pytest-cov",
+            "twine",
+        ]
+    },
+
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3 :: Only',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Scientific/Engineering :: Physics',
+        'Topic :: Utilities',
+    ],
+)
