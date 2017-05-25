7280c5bde70a62524cdb4cca105aa253f62c0633
Dongdong Tian <seisman.info@gmail.com>
2017-05-25 09:56:59 +0800

    python 3 is the future

--- docs/conf.py
+++ docs/conf.py
@@ -118,4 +118,4 @@ suppress_warnings = ['image.nonlocal_uri']
 
 
 # Example configuration for intersphinx: refer to the Python standard library.
-intersphinx_mapping = {'https://docs.python.org/': None}
+intersphinx_mapping = {'https://docs.python.org/3': None}
